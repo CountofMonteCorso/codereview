148d08ed6b023015a7c4bd56f29ab942c3364b64
Kevin <kcorso89@gmail.com>
2018-04-26 14:55:19 -0700

    module exports added

--- ex4.js
+++ ex4.js
@@ -1,15 +1,17 @@
-function openAll(openerFunction) {
-	for(var i = 0; i < 5; i += 1) {
-		setTimeout(() => {
-			openerFunction(i);
-		}, i * 1000);
+module.exports = function(arg1, arg2){
+	function openAll(openerFunction) {
+		for(var i = 0; i < 5; i += 1) {
+			setTimeout(() => {
+				openerFunction(i);
+			}, i * 1000);
 
-		function openGate(gateNumber) {
-			console.log(`Opening Gate #${gateNumber}`);
-		};
+			function openGate(gateNumber) {
+				console.log(`Opening Gate #${gateNumber}`);
+			};
 
+		}
 	}
-}
-openAll(openGate);
+	openAll(openGate);
 
-//My thinking is that you need to open the specific gate one at a time, corresponding to the gateNumber, meaning that would need to be a part of the for loop.  This way each time the loop completes, the program would open the next gate. I don't know how I can test this here though.
+	//My thinking is that you need to open the specific gate one at a time, corresponding to the gateNumber, meaning that would need to be a part of the for loop.  This way each time the loop completes, the program would open the next gate. I don't know how I can test this here though.
+};
--- ex5.js
+++ ex5.js
@@ -1,61 +1,63 @@
-var request = require("request");
+module.exports = function(arg1, arg2){
+	var request = require("request");
 
-//gets random joke
-function joke(error, response, body) {
+	//gets random joke
+	function joke(error, response, body) {
 
-	var options = {
-		url: "https://icanhazdadjoke.com/",
+		var options = {
+			url: "https://icanhazdadjoke.com/",
+			method: "GET",
+			headers: {
+		        "Accept" : "application/json"
+		        }
+		};
+
+		if (!error && response.statusCode == 200) {
+		    var info = JSON.parse(body);
+		}
+
+	}
+	request(options, function(err, res, body){
+		console.log(res.body);
+	});
+	 
+	//jokes by ID
+	function getJokeById (error, response, body){
+		var jokeID = process.env[2];
+		var options = {
+		url: "https://icanhazdadjoke.com/j/" + jokeID,
 		method: "GET",
 		headers: {
 	        "Accept" : "application/json"
 	        }
-	};
+	    };
 
-	if (!error && response.statusCode == 200) {
-	    var info = JSON.parse(body);
+		if (!error && response.statusCode == 200) {
+	   		var info = JSON.parse(body);
+	 	 }
 	}
+	request(options, function(err, res, body){
+		console.log(res.body);
+	});
 
-}
-request(options, function(err, res, body){
-	console.log(res.body);
-});
- 
-//jokes by ID
-function getJokeById (error, response, body){
-	var jokeID = process.env[2];
-	var options = {
-	url: "https://icanhazdadjoke.com/j/" + jokeID,
-	method: "GET",
-	headers: {
-        "Accept" : "application/json"
-        }
-    };
+	//search by term
+	function searchJokes (error, response, body){
+		var query = process.env[2];
+		var options = {
+		url: "https://icanhazdadjoke.com/search?term=" + query,
+		method: "GET",
+		headers: {
+	        "Accept" : "application/json"
+	        }
+	    };
 
-	if (!error && response.statusCode == 200) {
-   		var info = JSON.parse(body);
- 	 }
-}
-request(options, function(err, res, body){
-	console.log(res.body);
-});
+		if (!error && response.statusCode == 200) {
+	   		var info = JSON.parse(body);
+	 	 }
+	}
+	request(options, function(err, res, body){
+		console.log(res.body);
+	});
 
-//search by term
-function searchJokes (error, response, body){
-	var query = process.env[2];
-	var options = {
-	url: "https://icanhazdadjoke.com/search?term=" + query,
-	method: "GET",
-	headers: {
-        "Accept" : "application/json"
-        }
-    };
-
-	if (!error && response.statusCode == 200) {
-   		var info = JSON.parse(body);
- 	 }
-}
-request(options, function(err, res, body){
-	console.log(res.body);
-});
-
-//I know this doesn't quite work yet but I know it's close because I was able to get the first function to work. It wasn't until I added the others that it stopped working.  I think it's related to request being outside the function but when it's inside it doesn't work either. I don't know; it's super late and my brain is mush.
+	//I know this doesn't quite work yet but I know it's close because I was able to get the first function to work. It wasn't until I added the others that it stopped working.  I think it's related to request being outside the function but when it's inside it doesn't work either. I don't know; it's super late and my brain is mush.
+};
